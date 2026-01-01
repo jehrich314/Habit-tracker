6746013ea81ae2a6cf8cdec49bc2e4974fcdb3d6
jehrich314 <82480198+jehrich314@users.noreply.github.com>
2026-01-01 09:26:20 -0600

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE = "habits-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.webmanifest"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => (k === CACHE ? null : caches.delete(k)))))
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then(hit => hit || fetch(e.request))
+  );
+});
